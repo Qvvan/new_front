4933a367b9e9c9a1e0220858d5c39fefc0dcb7b7
Ivan Lebedev <90276875+Qvvan@users.noreply.github.com>
2025-06-17 14:56:36 +0300

    Update service-selector.js

--- js/components/service-selector.js
+++ js/components/service-selector.js
@@ -210,7 +210,7 @@ window.ServiceSelector = {
         setTimeout(() => {
             if (window.TGSLoader) {
                 const tgsPath = isActivated ?
-                    'assets/images/gifs/gift-opened.tgs' :
+                    'assets/images/gifs/gift-opened.png' :
                     'assets/images/gifs/gift-animate.tgs';
 
                 window.TGSLoader.loadTGSAnimation(
